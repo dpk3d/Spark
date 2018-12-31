d26b85bd3c6d05e82d75d504809e66926cd87fe7
Deepak Singh <39647054+dpk3d@users.noreply.github.com>
2018-12-31 15:58:41 +0530

    Adding more methods

--- SimpleDateTransformation.scala
+++ SimpleDateTransformation.scala
@@ -70,10 +70,82 @@ date.substring(6,8)
 
 def monthsBetween (start: Column, end:Column, inputFormat: String = YYYYMM): Column = {
 
-val startDate = to_date(unix_timestamp(start.substring(1,6),inputFormat).cast("timestamp")) 
-val endDate = to_date(unix_timestamp(end.substring(1,6), inputFormat).cast("timestamp")) 
+  val startDate = to_date(unix_timestamp(start.substring(1,6),inputFormat).cast("timestamp")) 
+  val endDate = to_date(unix_timestamp(end.substring(1,6), inputFormat).cast("timestamp")) 
 months_between (startDate, endDate).cast("Int")
 }
+  
+/* Add the Specified months for a given Date
+*
+*@note Providing a negative value substracts the months from dates
+*@param Column -> Initial / Given Date
+*@param monthsToAdd -> Months to be added
+*@param outputFormat -> Output format of the date required
+*@return -> Derived date with Additional Month Added
+*/
+  
+ def addMonths (column: Column, monthsToAdd: Int, outputFormat: String = YYYYMMDD) : Column = {
+    val stringToDate = to_date (unix_timstamp(column , YYYYMMDD).cast("timestamp"))
+   val endOfMonth = add_months (stringToDate , monthsToAdd)
+   date_format (endOfMonth, outputFormat)
+ } 
+  
+/* Subtracts the number of months for a given Date
+*
+*@note Providing a negative value substracts the months from dates
+*@param Date -> Input Date
+*@param monthsToSubtract -> Months to subtracts from the input date
+*@return -> Date After Substractions
+*/
+  
+def substractsMonths (Date: String , monthsToSubstract: Int = 1 ): String = {
+  var year = date.substring(0, 4)
+  var month = date.substring (4, 6)
+  
+  if (month  == 1) {
+      month = 12
+      year = year - 1
+  }
+    else {
+        month = month - 1
+    }
+  
+  "04d%02d".format( year, month)
+}
+  
+/*
+* Rerun the current times stamp
+*@param timestampFormat -> Required Output Timestamp Format
+*@return -> Return Date in required timestamp format
+*/
+  
+def currentTimeZone (timestampFormat: String ) : Column = {
+    from_unixtime( unix_timestamp, timestampFormat)
+}
+  
+/*
+* To check whether date is Valid or Not
+*@param Date -> Input Date
+*@return -> Boolean status, date is valid or not
+*/
+  
+  def isValidDate (Date : String) = Boolean = {
+      try {
+          var status = false
+          if (Date.trim.equalsIgnoreCase("") || Date.toInt == 0)
+            status = true
+        else if (Date.toInt != 0 ) {
+          val fmt = DateTimeFormat.forPattern( YYYYMMDD )
+            Try ( fmt.parseDateTime(Date)).isSuccess
+          status = Try (fmt.parseDateTime(Date)).isSuccess
+        }
+        status
+      }
+      catch {
+            case e: Exception => false
+      }
+  }
+  
 
 
 
