92f6b7e55e1c49868b15dd43e69527edfe074983
Deepak Singh <39647054+dpk3d@users.noreply.github.com>
2021-02-04 14:19:31 +0530

    Reading a flat file which has json data

---
+++ JsonDump.scala
@@ -0,0 +1,30 @@
+package com.singh.deepak.kakfa
+
+import org.apache.spark.sql.SparkSession
+
+/*
+* Reading Json Data from a flat file
+*
+**/
+
+object JsonDump {
+
+@transient lazy val logger: Logger = Logger.getLogger(this.getClass)
+
+val spark = SparkSession.builder().appName("Read Flat File").master("local").getOrCreate()
+val sc = spark.sparkContext
+
+val jsonDump = sc.textfile("Path to the flat file")
+
+val cnt = jsonDump.count
+
+val removeHeaderAndFooter = jsonDump.take(cnt.toInt).drop(1).dropRight(1)
+val rdd = sc.parallelize(removeHeaderAndFooter)
+val splitRdd = rdd.map(row => row.split("#")).map(x => (x(0) , x(1))
+val filterRdd = splitRdd.map(record => record._2.trim)
+
+val df = spark.read.option("multiLine", true).option("mode", "PERMISSIVE").json(filterRdd)
+df.printSchema
+df.show(false)
+
+}
